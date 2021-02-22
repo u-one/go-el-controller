3c6861b656bf4f0617fc84918740936ca02915fd
u-one <backflip270bb@gmail.com>
2021-02-22 17:37:38 +0900

    Fix script

--- deployments/deploy-pi.sh
+++ deployments/deploy-pi.sh
@@ -5,9 +5,9 @@ sudo apt-get -y install jq
 url=`curl https://api.github.com/repos/u-one/go-el-controller/releases/latest | jq '.assets[] | select(.name == "elexporter_linux_arm") | .browser_download_url' | sed 's/"//g'`
 
 echo $url 
-wget $url
+wget -q $url
 
-chmod +x elexporter_linux_arm
+chmod +x ./elexporter_linux_arm
 
 pkill elexporter
 
