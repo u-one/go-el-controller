39d3cd325b090682803c924cf91564fbbf5fbc51
u-one <backflip270bb@gmail.com>
2021-02-22 16:58:43 +0900

    Add -y option to install jq

--- deployments/deploy-pi.sh
+++ deployments/deploy-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install jq
+sudo apt-get -y install jq
 
 url=`curl https://api.github.com/repos/u-one/go-el-controller/releases/latest | jq '.assets[] | select(.name == "elexporter_linux_arm") | .browser_download_url' | sed 's/"//g'`
 
