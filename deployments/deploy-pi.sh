3ff47257662ff7e25e8cac2dc4ca57397615d278
u-one <backflip270bb@gmail.com>
2021-02-22 18:22:01 +0900

    Fix to start elexporter as service

--- deployments/deploy-pi.sh
+++ deployments/deploy-pi.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
+# setup
 sudo apt-get -y install jq
 
+# get latest binary
 url=`curl https://api.github.com/repos/u-one/go-el-controller/releases/latest | jq '.assets[] | select(.name == "elexporter_linux_arm") | .browser_download_url' | sed 's/"//g'`
-
 echo $url 
 wget -q $url
-
 chmod +x ./elexporter_linux_arm
 
+# stop service
+systemctl stop el-exporter.service
+
+# install
 DIR=/opt/u-one/el-exporter
-
 mkdir -p $DIR
-
 mv elexporter_linux_arm $DIR/elexporter
 mv el-exporter.service /etc/systemd/system/
 
 systemctl enable el-exporter.service
+
+# start service
+systemctl start el-exporter.service
 
